430b9dea98edec9105aa24025407612936a080ee
aluno <aluno@LAB-39.ic.ufba.br>
2024-04-08 17:54:02 -0300

    iniciando card vagas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { VagasComponent } from './components/vagas/vagas.component';
+import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent],
+  imports: [
+    RouterOutlet,
+    BrowserModule,
+    BrowserAnimationsModule,
+    HomeComponent, VagasComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
